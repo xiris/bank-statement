90a4ef38f35cfe9153fb7fcf3bda047e17b52676
xiris <chris@planner.net.br>
2021-05-23 14:59:47 +0200

    Adding auth, using Auth0 to generate tenant

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@
 extern crate diesel;
 
 use actix_web::{dev::ServiceRequest, App, HttpServer, web, Error};
+use actix_web_httpauth::extractors::bearer::{BearerAuth, Config};
+use actix_web_httpauth::extractors::AuthenticationError;
+use actix_web_httpauth::middleware::HttpAuthentication;
 use diesel::prelude::*;
 use diesel::r2d2::{self, ConnectionManager};
 
@@ -9,6 +12,7 @@ mod errors;
 mod handlers;
 mod models;
 mod schema;
+mod auth;
 
 pub type Pool = r2d2::Pool<ConnectionManager<SqliteConnection>>;
 
@@ -18,14 +22,15 @@ async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web-debug");
     let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
 
-    // connection pool
     let manager = ConnectionManager::<SqliteConnection>::new(database_url);
     let pool: Pool = r2d2::Pool::builder()
         .build(manager)
         .expect("Failed to connect to pool.");
 
     HttpServer::new(move || {
+        let auth = HttpAuthentication::bearer(validator);
         App::new()
+            .wrap(auth)
             .data(pool.clone())
             .route("/transactions", web::get().to(handlers::get_transactions))
             .route("/transactions/{id}", web::get().to(handlers::get_transaction_by_id))
@@ -35,4 +40,22 @@ async fn main() -> std::io::Result<()> {
     .bind("127.0.0.1:8081")?
     .run()
     .await
+}
+
+async fn validator(req: ServiceRequest, credentials: BearerAuth) -> Result<ServiceRequest, Error> {
+    let config = req
+        .app_data::<Config>()
+        .map(|data| data.clone())
+        .unwrap_or_else(Default::default);
+    
+    match auth::validate_token(credentials.token()) {
+        Ok(res) => {
+            if res == true {
+                Ok(req)
+            } else {
+                Err(AuthenticationError::from(config).into())
+            }
+        }
+        Err(_) => Err(AuthenticationError::from(config).into()),
+    }
 }
